e7c3dfcb86d8a542c7d0ecf27a9fe9108e5bf613
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2025-05-18 09:53:10 -0500

    feat(environment): add CurrentEnvironment class and EnvironmentEnum for environment management

---
+++ lib/core/current_environment.dart
@@ -0,0 +1,15 @@
+import 'package:integration_test/core/enums/environment_enum.dart';
+
+class CurrentEnvironment {
+  static EnvironmentEnum environment = EnvironmentEnum.development;
+
+  static bool get isMock => environment == EnvironmentEnum.mock;
+
+  static bool get isDevelopment => environment == EnvironmentEnum.development;
+
+  static bool get isProduction => environment == EnvironmentEnum.production;
+
+  static void setEnvironment(EnvironmentEnum env) {
+    environment = env;
+  }
+}
---
+++ lib/core/enums/environment_enum.dart
@@ -0,0 +1,1 @@
+enum EnvironmentEnum { production, development, mock }
