37fe3eeb243d1621778e05601cfc9ac6566923bc
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2025-05-18 17:42:30 -0500

    feat(keys): add keys for registration screen and its components

--- lib/core/consts/app_widget_keys.dart
+++ lib/core/consts/app_widget_keys.dart
@@ -7,6 +7,16 @@ class AppWidgetKeys {
   static const Key loginPasswordField = Key('login_password_field');
   static const Key loginButton = Key('login_button');
   static const Key loginErrorSnackBar = Key('login_error_snackbar');
+  static const Key loginRegisterButton = Key('login_register_button');
+  // Register Screen Keys
+  static const Key registerScreen = Key('register_screen');
+  static const Key registerNameField = Key('register_name_field');
+  static const Key registerEmailField = Key('register_email_field');
+  static const Key registerPasswordField = Key('register_password_field');
+  static const Key registerButton = Key('register_button');
+  static const Key registerErrorSnackBar = Key('register_error_snackbar');
+  static const Key registerSuccessSnackBar = Key('register_success_snackbar');
+  static const Key registerLoginButton = Key('register_login_button');
   // Home Screen Keys
   static const Key homeScreen = Key('home_screen');
   static const Key homeLogoutButton = Key('home_logout_button');
